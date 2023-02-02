12f78407a13302307dc14c7a47f779019a9f7a0e
brisalchert <brisalchert@gmail.com>
2023-02-02 10:53:07 -0500

    Bordered comments for methods for added clarity

--- Lab01/MyLinkedList.java
+++ Lab01/MyLinkedList.java
@@ -8,7 +8,9 @@ public class MyLinkedList {
     private MyLinkedList tail;
     private MyLinkedList current;
 
+    //------------------------------------------------------------------------------------------------------------------
     // Constructor: sets up a MyLinkedList node with val
+    //------------------------------------------------------------------------------------------------------------------
     public MyLinkedList(int val) {
         this.val = val;
         size = 1;
@@ -18,8 +20,10 @@ public class MyLinkedList {
         tail = this;
     }
 
+    //------------------------------------------------------------------------------------------------------------------
     // Returns the value of val at the node with the specified index in the linked list
     // Returns -1 if the index is invalid
+    //------------------------------------------------------------------------------------------------------------------
     public int get(int index) {
         // Check validity of index
         if (index > (size - 1) || index < 0) {
@@ -39,7 +43,9 @@ public class MyLinkedList {
         return current.val;
     }
 
+    //------------------------------------------------------------------------------------------------------------------
     // Adds a node with value val at the head of the linked list
+    //------------------------------------------------------------------------------------------------------------------
     public void addAtHead(int val) {
         MyLinkedList newNode = new MyLinkedList(val);
         size++;
@@ -54,7 +60,9 @@ public class MyLinkedList {
         newNode.next.prev = newNode;
     }
 
+    //------------------------------------------------------------------------------------------------------------------
     // Appends a node with value val at the tail of the linked list
+    //------------------------------------------------------------------------------------------------------------------
     public void addAtTail(int val) {
         MyLinkedList newNode = new MyLinkedList(val);
         size++;
@@ -69,9 +77,11 @@ public class MyLinkedList {
         tail = newNode;
     }
 
+    //------------------------------------------------------------------------------------------------------------------
     // Adds a node with value val at the specified index of the linked list
     // Appends to the end of the list if the index is equal to the length of the list
     // Does not insert the node if index is greater than the length of the list
+    //------------------------------------------------------------------------------------------------------------------
     public void addAtIndex(int index, int val) {
         // Check validity of index
         if (index > (size - 1) || index < 0) {
@@ -103,7 +113,9 @@ public class MyLinkedList {
         newNode.next.prev = newNode;
     }
 
+    //------------------------------------------------------------------------------------------------------------------
     // Deletes the node at the specified index in the list
+    //------------------------------------------------------------------------------------------------------------------
     public void deleteAtIndex(int index) {
 
     }
