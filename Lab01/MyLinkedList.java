f140f92a70adfd8f012b5530c1ec76700de9207a
brisalchert <brisalchert@gmail.com>
2023-02-02 09:08:39 -0500

    Set up methods and fields for MyLinkedList

---
+++ Lab01/MyLinkedList.java
@@ -0,0 +1,40 @@
+public class MyLinkedList {
+    private int val;
+    private MyLinkedList next;
+    private MyLinkedList prev;
+
+    // Constructor: sets up a MyLinkedList node with val = 0
+    public MyLinkedList() {
+        val = 0;
+        next = null;
+        prev = null;
+    }
+
+    // Returns the value of val at the node with the specified index in the linked list
+    // Returns -1 if the index is invalid
+    public int get(int index) {
+
+    }
+
+    // Adds a node with value val at the head of the linked list
+    public void addAtHead(int val) {
+
+    }
+
+    // Appends a node with value val at the tail of the linked list
+    public void addAtTail(int val) {
+
+    }
+
+    // Adds a node with value val at the specified index of the linked list
+    // Appends to the end of the list if the index is equal to the length of the list
+    // Does not insert the node if index is greater than the length of the list
+    public void addAtIndex(int index, int val) {
+
+    }
+
+    // Deletes the node at the specified index in the list
+    public void deleteAtIndex(int index) {
+
+    }
+}
