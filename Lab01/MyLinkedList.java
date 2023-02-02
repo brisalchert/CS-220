b44e58b6def2061cbc56005b225c5b16667cc0b1
brisalchert <brisalchert@gmail.com>
2023-02-02 09:20:30 -0500

    Added code to method get()

--- Lab01/MyLinkedList.java
+++ Lab01/MyLinkedList.java
@@ -1,19 +1,39 @@
 public class MyLinkedList {
     private int val;
+    private int index;
     private MyLinkedList next;
     private MyLinkedList prev;
+    private MyLinkedList head;
+    private MyLinkedList tail;
 
-    // Constructor: sets up a MyLinkedList node with val = 0
-    public MyLinkedList() {
-        val = 0;
+    // Constructor: sets up a MyLinkedList node with val
+    public MyLinkedList(int val) {
+        this.val = val;
         next = null;
         prev = null;
+        head = this;
+        tail = this;
     }
 
     // Returns the value of val at the node with the specified index in the linked list
     // Returns -1 if the index is invalid
     public int get(int index) {
+        MyLinkedList current = head;
+        this.index = 0;
 
+        // Traverse the list to the correct index
+        while (this.index > index) {
+            if (current.next == null) {
+                return -1;
+            }
+            else {
+                current = current.next;
+                this.index++;
+            }
+        }
+
+        // Return the value of val
+        return current.val;
     }
 
     // Adds a node with value val at the head of the linked list
