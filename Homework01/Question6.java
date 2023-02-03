60d8da69a32b897fa92ad96dfdb7192a6285c318
brisalchert <brisalchert@gmail.com>
2023-02-03 16:50:36 -0500

    Created algorithm for rotating the matrix 90 degrees clockwise

--- Homework01/Question6.java
+++ Homework01/Question6.java
@@ -7,14 +7,41 @@
 
 public class Question6 {
     public static void main (String[] args) {
-        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+        int temp1;
+        int temp2;
 
         // Print the input matrix
         System.out.print("Input matrix:\t");
         printMatrix(matrix);
         System.out.println();
 
+        // Rotate the matrix 90-degrees clockwise one "ring" at a time, starting from the outside
+        for (int n = 0; n < (matrix.length / 2); n++) {
+            // Rotate four values at a time until the entire "ring" is rotated
+            for (int m = n; m < (matrix.length - 1 - n); m++) {
+                // Store the top right corner in temp1
+                temp1 = matrix[m][matrix.length - 1 - n];
 
+                // Replace top right corner with top left corner
+                matrix[m][matrix.length - 1 - n] = matrix[n][m];
+
+                // Store the bottom right corner in temp2
+                temp2 = matrix[matrix.length - 1 - n][matrix.length - 1 - m];
+
+                // Replace bottom right corner with top right corner
+                matrix[matrix.length - 1 - n][matrix.length - 1 - m] = temp1;
+
+                // Store the bottom left corner in temp1
+                temp1 = matrix[matrix.length - 1 - m][n];
+
+                // Replace bottom left corner with bottom right corner
+                matrix[matrix.length - 1 - m][n] = temp2;
+
+                // Replace top left corner with bottom left corner
+                matrix[n][m] = temp1;
+            }
+        }
 
         // Print the output matrix
         System.out.print("Output matrix:\t");
