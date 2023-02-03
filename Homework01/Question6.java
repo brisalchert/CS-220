2650c66b0e602412b4bd823673dccff53f709598
brisalchert <brisalchert@gmail.com>
2023-02-03 15:09:31 -0500

    Set up Question6.java with header comment

---
+++ Homework01/Question6.java
@@ -0,0 +1,12 @@
+//----------------------------------------------------------------------------------------------------------------------
+//  Question6.java              Author: Brian Salchert
+//
+//  Problem: Design a program which rotates an n x n 2D matrix by 90-degrees clockwise. The rotation must be done
+//  directly, meaning the program cannot allocate another 2D matrix to perform the rotation.
+//----------------------------------------------------------------------------------------------------------------------
+
+public class Question6 {
+    public static void main (String[] args) {
+
+    }
+}
