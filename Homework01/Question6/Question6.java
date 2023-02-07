93a57ddb4af514d25fe3b9a81a54026b25dcb273
brisalchert <brisalchert@gmail.com>
2023-02-07 09:31:04 -0500

    Added package statement for Question6.java

--- Homework01/Question6/Question6.java
+++ Homework01/Question6/Question6.java
@@ -4,6 +4,7 @@
 //  Problem: Design a program which rotates an n x n 2D matrix by 90-degrees clockwise. The rotation must be done
 //  directly, meaning the program cannot allocate another 2D matrix to perform the rotation.
 //----------------------------------------------------------------------------------------------------------------------
+package Question6;
 
 public class Question6 {
     public static void main (String[] args) {
