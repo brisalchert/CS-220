c7f0ad7a2f132b08df0cff2156cfc50fc77123b2
brisalchert <brisalchert@gmail.com>
2023-02-07 08:56:31 -0500

    Added tests for index out of bounds checks

--- Lab01/LinkedListDriver.java
+++ Lab01/LinkedListDriver.java
@@ -11,17 +11,23 @@ public class LinkedListDriver {
         // Add 1 to the start of the list
         myLinkedList.addAtHead(1);
 
-        // Add 2 to the end of the list
-        myLinkedList.addAtTail(2);
+        // Add 1000 to the end of the list
+        myLinkedList.addAtTail(1000);
 
-        // Add 3 at index 1 in the list
-        myLinkedList.addAtIndex(1, 3);
+        // Add 600 at index 1 in the list
+        myLinkedList.addAtIndex(1, 600);
 
-        // Add 4 at index 0 in the list
-        myLinkedList.addAtIndex(0, 4);
+        // Attempt to add 5 at index 7 in the list
+        myLinkedList.addAtIndex(7, 5);
+
+        // Add 40 at index 0 in the list
+        myLinkedList.addAtIndex(0, 40);
 
         // Remove the item at index 3 in the list
         myLinkedList.deleteAtIndex(3);
+
+        // Attempt to remove the item at index 8 in the list
+        myLinkedList.deleteAtIndex(8);
 
         // Attempt to get the first 5 values in the list
         for (int index = 0; index < 5; index++) {
