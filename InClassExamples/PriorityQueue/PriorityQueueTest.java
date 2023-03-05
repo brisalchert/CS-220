e04b7e5186e4d861f7840ebaa9c842dca49cdaf4
brisalchert <brisalchert@gmail.com>
2023-03-05 12:46:29 -0500

    Added selction and insertion sorts and driver code

--- InClassExamples/PriorityQueue/PriorityQueueTest.java
+++ InClassExamples/PriorityQueue/PriorityQueueTest.java
@@ -1,11 +1,15 @@
 //----------------------------------------------------------------------------------------------------------------------
 //  PriorityQueueTest.java              Author: Brian Salchert
 //
-//  Driver class for the UnsortedPriorityQueue class.
+//  Driver class for Priority Queue classes and related sorting algorithms.
 //----------------------------------------------------------------------------------------------------------------------
 
 package PriorityQueue;
+
 import java.util.Comparator;
+import java.util.List;
+
+import TreeTest.LinkedPositionalList;
 
 public class PriorityQueueTest {
     public static void main(String[] args) {
@@ -27,6 +31,11 @@ public class PriorityQueueTest {
 
         UnsortedPriorityQueue<Integer,Integer> unsortedPriorityQueue = new UnsortedPriorityQueue<Integer,Integer>();
         SortedPriorityQueue<Integer,Integer> sortedPriorityQueue = new SortedPriorityQueue<Integer,Integer>();
+        LinkedPositionalList<Integer[]> unsortedList = new LinkedPositionalList<Integer[]>();
+        Integer[][] entryList = {{1, 5}, {7, 2}, {8, 1}, {3, 3}, {-2, 6}};
+        long startTime;
+        long endTime;
+        long runTime;
 
         unsortedPriorityQueue.insert(3, 4);
         unsortedPriorityQueue.insert(2, 5);
@@ -48,5 +57,53 @@ public class PriorityQueueTest {
         System.out.println(sortedPriorityQueue.size());
         System.out.println(sortedPriorityQueue.removeMin());
         System.out.println(sortedPriorityQueue.removeMin());
+
+        for (Integer[] entry : entryList) {
+            unsortedList.addLast(entry);
+        }
+
+        startTime = System.nanoTime();
+        selectionSortPQ(unsortedList);
+        endTime = System.nanoTime();
+
+        runTime = endTime - startTime;
+
+        System.out.println("Selection Sort runtime: " + runTime);
+
+        startTime = System.nanoTime();
+        insertionSortPQ(unsortedList);
+        endTime = System.nanoTime();
+
+        runTime = endTime - startTime;
+
+        System.out.println("Insertion Sort runtime: " + runTime);
+    }
+
+    public static LinkedPositionalList<Entry<Integer,Integer>> selectionSortPQ(LinkedPositionalList<Integer[]> list) {
+        UnsortedPriorityQueue<Integer,Integer> sortPQ = new UnsortedPriorityQueue<Integer,Integer>();
+
+        return doSort(list, sortPQ);
+    }
+
+    public static LinkedPositionalList<Entry<Integer,Integer>> insertionSortPQ(LinkedPositionalList<Integer[]> list) {
+        SortedPriorityQueue<Integer,Integer> sortPQ = new SortedPriorityQueue<Integer,Integer>();
+
+        return doSort(list, sortPQ);
+    }
+
+    private static LinkedPositionalList<Entry<Integer,Integer>> doSort(LinkedPositionalList<Integer[]> list,
+                                                                       AbstractPriorityQueue<Integer,Integer> pq) {
+        while (!list.isEmpty()) {
+            pq.insert(list.first().getElement()[0], list.first().getElement()[1]);
+            list.remove(list.first());
+        }
+
+        LinkedPositionalList<Entry<Integer,Integer>> finalList = new LinkedPositionalList<Entry<Integer,Integer>>();
+
+        while (!pq.isEmpty()) {
+            finalList.addLast(pq.removeMin());
+        }
+
+        return finalList;
     }
 }
