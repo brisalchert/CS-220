01f1df3d387ca548efb80f540b795d5a7968bf71
brisalchert <brisalchert@gmail.com>
2023-03-06 22:15:04 -0500

    Added driver code for LinkedBinaryTree.java

--- InClassExamples/TreeTest/TreeTest.java
+++ InClassExamples/TreeTest/TreeTest.java
@@ -6,13 +6,10 @@
 
 package TreeTest;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.ArrayList;
-
 public class TreeTest {
     public static void main(String[] args) {
         LinkedTree<Integer> tree = new LinkedTree<Integer>(1);
+        LinkedBinaryTree<Integer> binaryTree = new LinkedBinaryTree<Integer>(8);
 
         System.out.println(tree.isRoot(tree.root()));
 
@@ -24,5 +21,26 @@ public class TreeTest {
         for (Integer integer : tree) {
             System.out.println(integer);
         }
+
+        System.out.println(binaryTree.isRoot(binaryTree.root()));
+
+        // Create binary search tree (without binary search tree functionality)
+        Position<Integer> A = binaryTree.addLeftChild(binaryTree.root(), 5);
+        Position<Integer> B = binaryTree.addRightChild(binaryTree.root(), 11);
+        Position<Integer> C = binaryTree.addLeftChild(A, 2);
+        Position<Integer> D = binaryTree.addRightChild(A, 6);
+        Position<Integer> E = binaryTree.addLeftChild(C, 4);
+        Position<Integer> F = binaryTree.addLeftChild(D, 7);
+        Position<Integer> G = binaryTree.addLeftChild(B, 10);
+        Position<Integer> H = binaryTree.addRightChild(B, 12);
+        Position<Integer> I = binaryTree.addLeftChild(G, 9);
+        Position<Integer> J = binaryTree.addLeftChild(H, 14);
+        Position<Integer> K = binaryTree.addLeftChild(J, 13);
+
+        for (Integer integer : binaryTree) {
+            System.out.println(integer);
+        }
+
+        System.out.println("Size of binary tree: " + binaryTree.size());
     }
 }
