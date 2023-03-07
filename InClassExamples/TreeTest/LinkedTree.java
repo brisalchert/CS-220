b43143009b3148cad8fe5d45d678ac9b60c8b645
brisalchert <brisalchert@gmail.com>
2023-03-06 21:53:34 -0500

    Made the root field private

--- InClassExamples/TreeTest/LinkedTree.java
+++ InClassExamples/TreeTest/LinkedTree.java
@@ -97,7 +97,7 @@ public class LinkedTree<E> implements Tree<E> {
         return new Node<E>(e, parent, children);
     }
 
-    protected Node<E> root;
+    private Node<E> root;
     private int size;
 
     /**
