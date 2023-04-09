771cf5347500ccfdaacb21f03715fc3894ffb41d
Brian Salchert <brisalchert@gmail.com>
2023-04-09 13:19:21 -0400

    Added the merge method to Interval.java

--- InClassExamples/Interval/Interval.java
+++ InClassExamples/Interval/Interval.java
@@ -25,6 +25,22 @@ public class Interval implements Comparable<Interval> {
     }
 
     /**
+     * Support method for getting the start value of an interval
+     * @return the start value of the interval
+     */
+    private int getStart() {
+        return this.interval[0];
+    }
+
+    /**
+     * Support method for getting the end value of an interval
+     * @return the end value of the interval
+     */
+    private int getEnd() {
+        return this.interval[1];
+    }
+
+    /**
      * Compares two intervals and returns an integer value based on the result of the comparison
      * @param secondInterval the interval to be compared.
      * @return 1 if the first interval is after the second, -1 if the first interval is before the second, or 0 if
@@ -44,6 +60,25 @@ public class Interval implements Comparable<Interval> {
 
         // Return 0 if the two intervals are overlapping
         return 0;
+    }
+
+    /**
+     * Merges the current interval with another interval
+     * @param secondInterval the interval to merge with
+     * @return the merged interval
+     * @throws IllegalArgumentException if the two intervals do not overlap
+     */
+    public Interval merge(Interval secondInterval) throws IllegalArgumentException {
+        int[] mergedInterval = new int[2];
+
+        if (this.compareTo(secondInterval) != 0) {
+            throw new IllegalArgumentException("Invalid merge: Intervals are not overlapping");
+        }
+
+        mergedInterval[0] = Math.min(this.getStart(), secondInterval.getStart());
+        mergedInterval[1] = Math.max(this.getEnd(), secondInterval.getEnd());
+
+        return new Interval(mergedInterval);
     }
 
     /**
