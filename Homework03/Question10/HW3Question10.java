d67f33af71a20398d5d69f4a75ba905b5130280b
brisalchert <brisalchert@gmail.com>
2023-04-11 14:41:21 -0400

    Added functions for getting course order for valid prerequisites

--- Homework03/Question10/HW3Question10.java
+++ Homework03/Question10/HW3Question10.java
@@ -13,6 +13,7 @@
 
 package Question10;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 
 public class HW3Question10 {
@@ -20,24 +21,63 @@ public class HW3Question10 {
         int numCourses = 4;
         int[][] prerequisites = {{1,0},{2,0},{3,1},{3,2}};
 
-
+        System.out.println(getCourseOrder(numCourses, prerequisites));
     }
 
-    public LinkedList<Integer> getCourseOrder(int numCourses, int[][] prerequisites) {
-        // Initialize an adjacency matrix for a directed graph of the courses
-        int[][] graph = new int[numCourses][numCourses];
+    public static LinkedList<Integer> getCourseOrder(int numCourses, int[][] prerequisites) {
+        LinkedList<Integer> courseOrder = new LinkedList<>();
 
-        // Fill in the adjacency matrix for each prerequisite
-        for (int[] prerequisite : prerequisites) {
-            int nextCourse = prerequisite[0];
-            int prevCourse = prerequisite[1];
-
-            // Add an edge from the previous course to the next course
-            graph[prevCourse][nextCourse] = 1;
+        // Initialize a HashMap with keys of course numbers and values of "unlocked" courses
+        HashMap<Integer,LinkedList<Integer>> unlockMap = new HashMap<>(numCourses);
+        for (int course = 0; course < numCourses; course++) {
+            unlockMap.put(course, new LinkedList<>());
         }
 
-        // Find a course with no prerequisites
+        // Initialize a HashMap with keys of course numbers and values of their prerequisites
+        HashMap<Integer, LinkedList<Integer>> prerequisiteMap = new HashMap<>(numCourses);
+        for (int course = 0; course < numCourses; course++) {
+            prerequisiteMap.put(course, new LinkedList<>());
+        }
 
-        return null;
+        // Populate HashMaps with info from prerequisite array
+        for (int[] prerequisite : prerequisites) {
+            int followingCourse = prerequisite[0];
+            int prerequisiteCourse = prerequisite[1];
+
+            // Add to unlockMap
+            unlockMap.get(prerequisiteCourse).add(followingCourse);
+
+            // Add to prerequisiteMap
+            prerequisiteMap.get(followingCourse).add(prerequisiteCourse);
+        }
+
+        // Take the courses in the correct order
+        takeCourses(unlockMap, prerequisiteMap, courseOrder, numCourses);
+
+        return courseOrder;
+    }
+
+    public static void takeCourses(HashMap<Integer,LinkedList<Integer>> unlockMap,
+                                   HashMap<Integer,LinkedList<Integer>> prerequisiteMap,
+                                   LinkedList<Integer> courseOrder,
+                                   int numCourses) {
+        // Add all courses that have no remaining prerequisites
+        for (Integer course : prerequisiteMap.keySet()) {
+            if (prerequisiteMap.get(course).isEmpty() && !courseOrder.contains(course)) {
+                // Add course to the final list
+                courseOrder.add(course);
+
+                // Remove fulfilled prerequisites from other courses
+                for (Integer followingCourse : unlockMap.get(course)) {
+                    // Remove the taken course from the prerequisites
+                    prerequisiteMap.get(followingCourse).remove(course);
+                }
+            }
+        }
+
+        // If courses remain to be taken, call recursively
+        if (courseOrder.size() < numCourses) {
+            takeCourses(unlockMap, prerequisiteMap, courseOrder, numCourses);
+        }
     }
 }
